6d3be547aa7d3dfe16dc976573475347a83e043a
sefogi <sebast18@gmail.com>
2023-10-04 23:22:12 +0200

    clases de python nuevas

--- variables 2.0/desempaquetados.py
+++ variables 2.0/desempaquetados.py
@@ -1,1 +1,12 @@
-#
+#es una forma de asignar valores a variables de manera diferente y particular
+#// en el caso de conjutos no permite desempaquetar datos //
+# creamos una tupla
+datos = ("Lucas", "Dalto", 100000)
+
+#creamos una lista
+datos_lista = ["Lucas", "Dalto", 100000]
+
+# aqui cremos dos variables que desencapsulan la tupla ej: nombre = Lucas/ apellidos = Dalto
+nombre, apellidos, suscriptores = datos_lista
+
+print(suscriptores)
